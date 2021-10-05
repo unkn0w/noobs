1f745bcc8ffbb911726588a5792d50e3779d20db
unkn0w <unknow@uw-team.org>
2021-10-05 13:20:25 +0200

    dodałem skrypt stawiający LAMP

---
+++ scripts/chce_LAMP.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# LAMP = Linux + Apache + MySQL (MariaDB) + PHP
+# Autor: Jakub 'unknow' Mrugalski
+
+apt update
+apt install -y software-properties-common 
+
+# Repozytoria zewnętrzne z PHP 8.0 i najnowszym Apachem (nie ma ich w standardowym Ubuntu)
+add-apt-repository -y ppa:ondrej/apache2
+add-apt-repository -y ppa:ondrej/php
+
+# apache + najpopularniejsze moduły do PHP
+apt -y install apache2 php8.0 libapache2-mod-php8.0 php8.0-zip php8.0-xml php8.0-sqlite3 php8.0-pgsql php8.0-mysql php8.0-mcrypt php8.0-mbstring php8.0-intl php8.0-gd php8.0-curl php8.0-cli php8.0-bcmath
+
+# aktywacja mod_rewrite dla wspierania krótkich linków - np. w Wordpress
+a2enmod rewrite
+
+# restart usługi po dodaniu nowego modułu
+systemctl restart apache2
+
+# Usuwamy domyślną 
+rm /var/www/html/index.html
+
+# Dowód na działanie PHP
+echo '<?php echo "2 + 2 = ".(2+2); ' >/var/www/html/index.php
+
+# == Lekki hardening ustawień ==
+
+# Serwer ma się nie doklejać swojej stopki nigdzie
+sed -i -e "s/^ServerSignature OS*.*\$/ServerSignature Off/" '/etc/apache2/conf-available/security.conf'
+
+# Serwer będzie się przedstawiał jako "Apache" - bez wersji softu i OS
+sed -i -e "s/^ServerTokens OS*.*\$/ServerTokens Prod/" '/etc/apache2/conf-available/security.conf'
+
