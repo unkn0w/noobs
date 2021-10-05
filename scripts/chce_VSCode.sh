5b980a3c2b4292cc57da55a69eb0f6279000da91
Simon Hryszko <simon@hryszko.dev>
2021-10-05 22:47:06 +0200

    Fix about vscode what should not be run as sudo

--- scripts/chce_VSCode.sh
+++ scripts/chce_VSCode.sh
@@ -3,9 +3,9 @@
 # Autor: Jakub 'unknow' Mrugalski
 
 # Check if user is root
-if [ "$(id -u)" != "0" ]; then
-	echo "This script must be run as root" 1>&2
-	echo "Try: sudo $0" 1>&2
+if [ "$(id -u)" == "0" ]; then
+	echo "This script cannot be run as root" 1>&2
+	echo "Try: $0" 1>&2
 	exit 1
 fi
 
