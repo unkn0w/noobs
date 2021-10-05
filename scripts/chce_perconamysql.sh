fd2c52ac18fef4cbe3720a2ca15386bbbb2b1e6c
FX <ferexio@users.noreply.github.com>
2021-10-06 00:51:44 +0200

    Update chce_perconamysql.sh

--- scripts/chce_perconamysql.sh
+++ scripts/chce_perconamysql.sh
@@ -1,7 +1,6 @@
-checkuser=$(echo whoami)
 checkport=$(awk '{print substr($1,2); }' /etc/hostname)
 
-if [[ $checkuser != root ]]; then
+if [ "$(id -u)" == "0" ]; then
 
 cd $HOME && wget https://downloads.percona.com/downloads/Percona-Server-5.7/Percona-Server-5.7.35-38/binary/tarball/Percona-Server-5.7.35-38-Linux.x86_64.glibc2.12-minimal.tar.gz && tar -xzvf Percona-Server-5.7.35-38-Linux.x86_64.glibc2.12-minimal.tar.gz && rm Percona-Server-5.7.35-38-Linux.x86_64.glibc2.12-minimal.tar.gz && mv Percona-Server-5.7.35-38-Linux.x86_64.glibc2.12-minimal/ mysql/ && mkdir mysql/mysql_secure
 
