3aa11aeba427332852b414f9a79048de902eb4d4
Radoslaw Karasinski <radoslaw.karasinski@gmail.com>
2021-10-06 12:50:35 +0200

    Zmiana nazwy zmiennej na uppercase

--- scripts/chce_usera.sh
+++ scripts/chce_usera.sh
@@ -2,20 +2,20 @@
 # Tworzenie nowego uzytkownika, z dostepem do sudo i kopia authorized_keys
 # Autor: Radoslaw Karasinski
 
-read -p "Podaj nazwe nowego uzytkownika: " username
+read -p "Podaj nazwe nowego uzytkownika: " USERNAME
 
-if sudo id "$username" &>/dev/null; then
+if sudo id "$USERNAME" &>/dev/null; then
     echo "Podany uzytkownik juz istnieje!"
     exit
 fi
 
 # stworz nowego uzytkownika
-sudo adduser $username
+sudo adduser $USERNAME
 
 # dodaj nowego uzytkownika do sudo
-sudo usermod -aG sudo $username
+sudo usermod -aG sudo $USERNAME
 
-SSH_DIR="/home/$username/.ssh"
+SSH_DIR="/home/$USERNAME/.ssh"
 
 # stworz folder na ustawienia ssh oraz ustaw odpowiednie prawa
 sudo mkdir -p $SSH_DIR
@@ -26,7 +26,7 @@ sudo touch $SSH_DIR/authorized_keys
 sudo chmod 600 $SSH_DIR/authorized_keys
 
 # zmien wlasciciela folderu i plikow
-sudo chown -R $username:$username $SSH_DIR
+sudo chown -R $USERNAME:$USERNAME $SSH_DIR
 
 # skopiuj klucze obecnego uzytkownika do nowo stworzoneg
 cat ~/.ssh/authorized_keys | sudo tee -a $SSH_DIR/authorized_keys >/dev/null
