19398218d948d93fd986a2ef5cb04410fbdfd629
Jakub <kubasurdej@gmail.com>
2021-10-07 22:13:53 +0200

    iImprove LAMP test:wq

--- tests/chce_LAMP_test.sh
+++ tests/chce_LAMP_test.sh
@@ -2,4 +2,13 @@
 
 set -euxo pipefail
 
-echo $USER
+apt install curl
+EXPECTED_RESPONSE="2 + 2 = 4"
+RESPONSE=$(curl -Ss localhost)
+
+if [ "$EXPECTED_RESPONSE" = "$RESPONSE" ]; then
+    echo "Response test passed"
+else
+    echo "Wrong response from server. Expected $EXPECTED_RESPONSE, received $RESPONSE"
+    exit 1
+fi
