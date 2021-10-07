dfe35f10cf74732105c9e119929b151b41a161cb
Grzegorz Ćwikliński <19205612+Hunqer@users.noreply.github.com>
2021-10-07 12:34:25 +0200

    Add fixes related to review

--- actions/chce_usera.sh
+++ actions/chce_usera.sh
@@ -2,48 +2,26 @@
 # Tworzenie nowego uzytkownika, z dostepem do sudo i kopia authorized_keys
 # Autor: Radoslaw Karasinski, Grzegorz Ćwikliński
 
-
-# Kolorki, zeby bylo ladniej :)
-if [[ -t 1 ]]
-then
-    ncolors="$(tput colors)"
-    if [[ -n "${ncolors}" ]] && [[ "${ncolors}" -ge 8 ]]
-    then
-        C_RED='\e[1;31m'
-        C_PURPLE='\e[1;35m'
-        C_YELLOW='\e[1;33m'
-        C_GREEN='\e[1;32m'
-        C_BLUE='\e[1;34m'
-        C_CLEAR='\e[0m'
-    fi
-fi
-
-_info() { echo -e "${C_GREEN}LOG [INFO] -> $*${C_CLEAR}" >&2; }
-_debug() { echo -e "${C_BLUE}LOG [DEBUG] -> $*${C_CLEAR}" >&2; }
-_warn() { echo -e "${C_YELLOW}LOG [WARNING] -> $*${C_CLEAR}" >&2;}
-_error() { echo -e "${C_RED}LOG [ERROR] -> $*${C_CLEAR}" >&2; exit 1; }
-_source() { echo "$*"; }
-
-
-
 _check_if_user_exits() {
     given_user=$1
     if sudo id "${given_user}" &>/dev/null; then
-            _error "Użytkownik ${given_user} już istnieje!"
+            echo "Użytkownik ${given_user} już istnieje!"
+            exit 1
     fi
 }
 
 _check_if_user_blank() {
     given_user=$1
     if [ -z "$1" ]; then
-        _error "Nie podałeś nazwy użytkownia!"
+        echo "Nie podałeś nazwy użytkownia!"
+        exit 1
 fi
 }
 
 if ! [ -z "$1" ]; then
     username=$1
 else
-    _info "Podaj nazwę użytkownika:"
+    echo "Podaj nazwę użytkownika:"
     read username
 fi
 
@@ -72,4 +50,4 @@ sudo chown -R $username:$username $ssh_dir
 # skopiuj klucze obecnego uzytkownika do nowo stworzoneg
 cat ~/.ssh/authorized_keys | sudo tee -a $ssh_dir/authorized_keys >/dev/null
 
-_info "Pomyślnie stworzono użytkownia ${username}."
+echo "Pomyślnie stworzono użytkownia ${username}."
