0c6fffbc03aa8da071239d5fb2505e98eb68af32
root <root@e604.mikr.us>
2021-10-12 18:56:17 +0000

    Add support for additional parameters

--- scripts/chce_netdata.sh
+++ scripts/chce_netdata.sh
@@ -1,23 +1,53 @@
 #!/bin/bash
 # Create netdata instance
 # Autor: Radoslaw Karasinski
+# Supported parameters:
+#  --port PORT_NUMBER,
+#  --token TOKEN
+#  --rooms ROOM
+#  --url URL
+#  --duplicate
 
 
-if [ -n "$1" ]; then
-    port=$1
-else
+# some bash magic: https://brianchildress.co/named-parameters-in-bash/
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+    fi
+  shift
+done
+
+
+if ! [ -n "$port" ]; then
     echo "Give desired port for netdata: (i.e. 20xxx or 30xxx):"
     read -r port
 fi
 
+extra_args=()
+
+if [ -n "$token" ]; then
+    extra_args+=(--claim-token $token)
+fi
+
+if [ -n "$rooms" ]; then
+    extra_args+=(--claim-rooms $rooms)
+fi
+
+if [ -n "$url" ]; then
+    extra_args+=(--claim-url $url)
+fi
+
+if [ -n "$duplicate" ]; then
+    extra_args+=(--allow-duplicate-install)
+fi
 
 echo "Install required packages."
 apt install -y curl
 echo
 
-
 # install netdata
-bash <(curl -Ss https://my-netdata.io/kickstart.sh) --allow-duplicate-install
+bash <(curl -Ss https://my-netdata.io/kickstart.sh) "${extra_args[@]}"
 
 # change default netdata port and restart service
 sed -i "s|# default port = 19999|default port = $port|" /etc/netdata/netdata.conf
