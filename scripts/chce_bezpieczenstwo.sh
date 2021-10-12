1e8f4fce32894411cf4773e9c427e5d73afc50c3
asc3rr <borysgnacinski@gmail.com>
2021-10-12 22:11:24 +0200

    added users checking(users_other_than_root), changed MaxAuthTries

--- scripts/chce_bezpieczenstwo.sh
+++ scripts/chce_bezpieczenstwo.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 #Author Borys Gnaciński
 
-# ssh hardening
-echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
-echo "AuthorizedKeysFile      .ssh/authorized_keys" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo "AllowGroups ssh_group" >> /etc/ssh/sshd_config
-echo "X11Forwarding no" >> /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "MaxAuthTries 6" >> /etc/ssh/sshd_config
+users_other_than_root(){
+    if [ "$(sudo getent passwd | grep /bin/bash | awk -F: '{print $1}' | grep -v root)" != "" ]
+    then
+        return true
+    else
+        return false
+    fi 
+}
+
+# ssh securing
+securing_ssh(){
+    echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
+    echo "AuthorizedKeysFile      .ssh/authorized_keys" >> /etc/ssh/sshd_config
+    echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+    echo "AllowGroups ssh_group" >> /etc/ssh/sshd_config
+    echo "X11Forwarding no" >> /etc/ssh/sshd_config
+    echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+    echo "MaxAuthTries 3" >> /etc/ssh/sshd_config
+}
