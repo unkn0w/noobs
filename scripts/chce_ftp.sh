d055178f2d493a4c3a5070df84703783a705ab01
Mariusz Kowalski <maniek205@gmail.com>
2021-10-15 07:23:05 +0000

    Added port detection. Added user check Removed installation of systemctl

--- scripts/chce_ftp.sh
+++ scripts/chce_ftp.sh
@@ -2,10 +2,33 @@
 # FTP installation script
 # Authors: Mariusz 'maniek205' Kowalski
 
-apt update
-apt install -y vsftpd systemctl
-sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
-systemctl enable vsftpd
-systemctl restart vsftpd
+[ "$EUID" -eq 0 ] && { err "Uruchamianie jako root jest niebezpieczne. Uzyj zwyklego uzytkownika."; }
+sudo --validate || { err "Nie masz uprawnien do uruchamiania komend jako root - dodaj '$USER' do grupy 'sudoers'."; }
 
-echo "FTP server has installed. Use your credentials to log in on port 21"
+hostname=$(hostname)
+listen_port=20${hostname:1}
+
+if sudo lsof -i:"${listen_port}" | grep -q PID ; then
+   echo "$listen_port in use trying: "
+   listen_port=30${hostname:1}
+   echo "$listen_port"
+elif sudo lsof -i:"${listen_port}" | grep -q PID ; then
+   echo "$listen_port in use error. All external ports are in use. Please release external port 20${hostname:1} or 30${hostname:1}"
+   exit 1
+fi
+echo "Using port: $listen_port"
+
+sudo apt update
+sudo apt install -y vsftpd
+sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
+
+sudo echo "
+listen_port=${listen_port}
+" >> /etc/vsftpd.conf
+
+sudo systemctl enable vsftpd
+sudo systemctl restart vsftpd
+
+echo "FTP server has been installed. Use your credentials to log in.
+Server IP: srvX.mikr.us (change X to your server number)
+Port: $listen_port"
