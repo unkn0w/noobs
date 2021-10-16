10e98a75d32662437ea8210493f1090ef80451c4
Paweł Kamiński <pawel13k@gmail.com>
2021-10-16 16:48:37 +0200

    fix: Zmiana sposobu interakcji z HAProxy na systemd i dodatkowa weryfikacja konfiguracji przed jej zapisaniem

--- scripts/chce_loadbalancer.sh
+++ scripts/chce_loadbalancer.sh
@@ -28,7 +28,7 @@ _ask_input() {
 _service_exists() {
     local service=$1
 
-    if [[ -f "/etc/init.d/$service" ]]; then
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$service.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $service.service ]]; then
         return 0
     else
         return 1
@@ -58,6 +58,20 @@ else
     apt update
     apt install -y haproxy
     echo -e "[\e[32m OK \e[39m] zainstalowane"
+fi
+
+
+
+##########
+# Sprawdzanie obecnej konfiguracji
+#
+
+haproxy -c -V -f /etc/haproxy/haproxy.cfg 
+if [ $? -ne 0 ]; then
+  echo -e "[\e[31m ERR \e[39m] Twoja obecna konfiguracja serwera HAProxy jest niepoprawna"
+  echo -e "[\e[31m ERR \e[39m] Sprawdź plik /etc/haproxy/haproxy.cfg"
+
+  exit 1
 fi
 
 
@@ -141,13 +155,28 @@ do
   ((i=i+1))
 done
 
+
+# Sprawdzanie nowej konfiguracji
+tmpConfig=/tmp/${randomName}-haproxy.cfg
+cp /etc/haproxy/haproxy.cfg ${tmpConfig}
+echo "$config" >> ${tmpConfig}
+haproxy -c -V -f ${tmpConfig}
+configReturn=$?
+rm ${tmpConfig}
+if [ $configReturn -ne 0 ]; then
+  echo -e "[\e[31m ERR \e[39m] Niestety podana konfiguracja jest niepoprawna"
+
+  exit 1
+fi
+
+
 # Dodanie nowego wpisu do konfiguracji
 echo "$config" >> /etc/haproxy/haproxy.cfg
 echo -e "[\e[32m OK \e[39m] Twoja konfiguracja została zapisana w: /etc/haproxy/haproxy.cfg"
 
 # Restart servera HAProxy
 echo -e "[\e[34m i \e[39m] Restart serwera"
-/etc/init.d/haproxy restart
+systemctl restart haproxy
 
 
 echo -e "[\e[32m OK \e[39m] Gotowe!"
