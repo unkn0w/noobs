34bce9c79d26e295b0394024c175e533d3b0075f
Artur <artur@deep.co.uk>
2021-10-17 18:11:05 +0200

    syntax error fixed, redundant code fragments removed

--- actions/chce_usera.sh
+++ actions/chce_usera.sh
@@ -24,10 +24,11 @@ _check_if_user_blank() {
     if [ -z "$1" ]; then
         echo "Nie podałeś nazwy użytkownia!"
         exit 1
-fi
+    fi
+}
 
 _password_get(){
-        username_arg=$1
+  username_arg=$1
 	while true; do
                 if [ "$username_arg" -eq "0" ]; then
 		        # ask for password
@@ -69,50 +70,7 @@ _password_get $username_arg
 
 
 # stworz nowego uzytkownika
-sudo useradd -m -p "$password" "$username"
-
-# Ask for the new user name
-if [ $# -eq 0 ]
-then
-	read -p "Podaj nazwe nowego uzytkownika: " username
-else
-	username=$(echo $1)
-fi
-
-# exit if the user doesn't exists
-if sudo id "$username" &>/dev/null; then
-    echo "Podany uzytkownik juz istnieje!"
-    exit
-fi
-
-read -p "Czy chcesz podać hasło? Jeśli nie zostanie ono wygenerowane [T/n] " c
-# if $c is 'n'
-if [[ "$c" == "n" ]]; then
-	password=$(head -c255 /dev/urandom | base64 | grep -Eoi '[a-z0-9]{12}' | head -n1)
-else
-	while true; do
-		# ask for password
-		read -sp 'Podaj hasło: ' password
-		echo
-		read -sp 'Podtórz hasło: ' password_repeat
-		echo
-
-		# if passwords are equal
-		if [ "$password" == "$password_repeat" ]; then
-			break
-		else
-			echo "Hasła się nie zgadzają, spróbuj ponownie!"
-		fi
-	done
-fi
-
-# stworz nowego uzytkownika
-useradd -m -p "$password" "$username" && echo "Uzytkownik $username zostal stworzony"
-
-# If password generated, then echo then
-if [[ "$c" == "n" ]]; then
-	echo "Hasło: $password"
-fi
+sudo useradd -m -p "$password" "$username" && echo "Uzytkownik $username zostal stworzony"
 
 # dodaj nowego uzytkownika do sudo
 sudo usermod -aG sudo $username
