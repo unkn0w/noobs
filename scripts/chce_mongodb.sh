b8a6c2c0819ee69da52582c2743afcb48c5b9d15
kacper <kacper.adamczak@gmail.com>
2021-10-18 14:56:17 +0000

    Poprawa konstrukcji warunków wykonywania bloków

--- scripts/chce_mongodb.sh
+++ scripts/chce_mongodb.sh
@@ -5,11 +5,7 @@
 #
 
 
-{
-    wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-} || {
-    sudo apt-get install gnupg; wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-}
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - && { printf "Prawidłowo zaimportowano klucz do repozytorium MongoDB"; } || { sudo apt-get install gnupg; wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - ;  printf "\nZainstalowano pakiet gnugp oraz prawidłowo zaimportowano klucz do repozytorium MongoDB\n";}
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
@@ -17,14 +13,9 @@ sudo apt-get update
 
 sudo apt-get install -y mongodb-org
 
-{
-    sudo systemctl start mongod
-} || {
-    sudo systemctl daemon-reload
-    sudo systemctl start mongod
+sudo systemctl start mongod && { printf "\nPrawidłowo uruchomiono MongoDB\n";} || { sudo systemctl daemon-reload; sudo systemctl start mongod
 }
 
-echo "MongoDB jest poprawnie zainstalowana i uruchomiona"
-
+printf "\nMongoDB jest poprawnie zainstalowana i uruchomiona\n"
 
 #sudo npm install -g mongo-express
