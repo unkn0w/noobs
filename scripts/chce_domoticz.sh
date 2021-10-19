24038bc080c7df3c93e7e3e3e83250fb0e4215a5
FX <ferexio@users.noreply.github.com>
2021-10-19 11:07:07 +0200

    Adding additional and required packages

--- scripts/chce_domoticz.sh
+++ scripts/chce_domoticz.sh
@@ -18,13 +18,16 @@ EOF
 )
 #===== Checker =====
 if [ -z "$1" ]; then
-echo "Poprawna składnia: ./chce_domoticz.sh port_http port_https"
+    echo "Poprawna składnia: ./chce_domoticz.sh port_http port_https"
 elif [ -z "$2" ]; then
-echo "Poprawna składnia: ./chce_domoticz.sh port_http port_https"
+    echo "Poprawna składnia: ./chce_domoticz.sh port_http port_https"
 else
+#===== Install required packages =====
+apt update
+apt install libusb-0.1-4 libcurl3-gnutls tar wget lsb -y
 #===== Script =====
 mkdir /opt/domoticz
-cd /opt/domoticz
+cd /opt/domoticz || { echo 'Folder nie istnieje'; exit; }
 wget --inet4-only https://releases.domoticz.com/releases/release/domoticz_linux_x86_64.tgz
 tar -xzvf domoticz_linux_x86_64.tgz
 rm domoticz_linux_x86_64.tgz
