9ee4d97cd367502e7f6a3b61f92c2b3f72516e82
Jakub 'unknow' Mrugalski <unknow@uw-team.org>
2021-10-21 14:55:20 +0200

    poprawki przy wywoływaniu sudo

--- scripts/chce_zsh.sh
+++ scripts/chce_zsh.sh
@@ -5,7 +5,7 @@
 sudo apt update && sudo apt install zsh git
 
 # instalacja oh-my-zsh
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O- | sudo sh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O- | sh
 
 # instalacja zewnętrznych, niestandardowych rozszerzeń
 git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/plugins/zsh-z
@@ -15,4 +15,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plug
 sed -i 's|plugins=.*|plugins=(git zsh-z docker docker-compose sudo zsh-syntax-highlighting ufw ubuntu screen)|' ~/.zshrc
 
 # ustawienie ZSH jako domyślnego shella dla aktualnego użytkownika
-chsh -s /bin/zsh "$USER"
+sudo chsh -s /bin/zsh "$USER"
