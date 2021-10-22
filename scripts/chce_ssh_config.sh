a56e130d62693f8f7e681880d3d2b8dbe2b23fbb
root <root@e604.mikr.us>
2021-10-22 14:53:27 +0000

    Init version of script - parsing arguments

---
+++ scripts/chce_ssh_config.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Create ssh config for easy connection with mikr.us
+# Autor: Radoslaw Karasinski
+# Usage: you can pass following arguments:
+#   --mikrus MIKRUS_NAME (i.e. 'X123')
+#   --user USERNAME (i.e. 'root')
+#   --port PORT_NUMBER (to configure connection for non mikr.us host)
+#   --host HOSTNAME (to configure connection for non mikr.us host)
+
+# some bash magic: https://brianchildress.co/named-parameters-in-bash/
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare "$param"="$2"
+        shift 1
+    else
+        if [[ $1 == *"@"* ]]; then
+          possible_ssh_param=$1
+        fi
+    fi
+  shift
+done
+
+port="${port:-22}"
+user="${user:-root}"
+
+if [ -n "$mikrus" ]; then
+    if ! [[ "$mikrus" =~ [a-q][0-9]{3}$ ]]; then
+        echo "--mikrus parameter is not valid!"
+        exit 1
+    fi
+
+    port="$(( 10000 + $(echo $mikrus | grep -o '[0-9]\+') ))"
+
+    key="$(echo $mikrus | grep -o '[^0-9]\+' )"
+    declare -A hosts
+    hosts["a"]="srv03"
+    hosts["b"]="srv04"
+    hosts["e"]="srv07"
+    hosts["f"]="srv08"
+    hosts["g"]="srv09"
+    hosts["h"]="srv10"
+    hosts["q"]="mini01"
+    host="${hosts[$key]}"
+    if [ -z "$host" ]; then
+        echo "Server hostname not known for key $key"
+        exit 1
+    fi
+    host="$host.mikr.us"
+fi
+
+if [ -n "$possible_ssh_param" ]; then
+    user="${possible_ssh_param%%@*}"
+    host="${possible_ssh_param#*@}"
+fi
+
+
+
+if [ -z "$host" ]; then
+    echo "Host was not recognized by any known method (--mikrus or --host or by specifying user@host.com"
+    exit 2
+fi
+
+echo "Following params will be used to generate ssh config: user:'$user', host:'$host', port:'$port'"
+read -n 1 -s  -p "Press enter (or space) to continue or any other key to cancel." decision
+echo ""
+if [ -n "$decision" ]; then
+    echo "No further changes"
+    exit 0
+fi
+
+echo "continue"
