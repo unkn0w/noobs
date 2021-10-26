96c15a8d8639b50e4a7ef5b73c9d152304b23984
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 16:22:58 +0200

    Migrated chce_ruby.sh from rbenv to asdf version manager

--- scripts/chce_ruby.sh
+++ scripts/chce_ruby.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
-# rbenv + ruby
+# asdf + ruby
 # Autor: Mikołaj Kamiński (mikolaj-kaminski.com)
-
+​
 if [[ $EUID -ne 0 ]]; then
     echo -e "W celu instalacji tego pakietu potrzebujesz wyzszych uprawnien! Uzyj polecenia \033[1;31msudo ./chce_ruby.sh\033[0m lub zaloguj sie na konto roota i wywolaj skrypt ponownie."
     exit 1
 fi
-
-# Instalacja rbenv
+​
+# Instalacja Git
 apt update
-apt install ruby-build rbenv -y
-
-# Konfiguracja rbenv
+apt install -y git
+​
+# Instalacja asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+​
+# Konfiguracja asdf
 if [ -n "$ZSH_VERSION" ]; then
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
-    echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+    echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+    echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
     source ~/.zshrc
 elif [ -n "$BASH_VERSION" ]; then
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+    echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
     source ~/.bashrc
 else
     echo "Uzywasz powloki innej niz Zsh i Bash. Do poprawnego dzialania rbenv koniecznie jest dodanie do skryptu uruchomieniowego Twojej powloki nastepujacych polecen:"
-    echo "export PATH=\"\$HOME/.rbenv/bin:\$PATH\""
-    echo "eval \"\$(rbenv init -)\""
+    echo '. $HOME/.asdf/asdf.sh'
+    echo '. $HOME/.asdf/completions/asdf.bash'
 fi
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
+​
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+​
+# Dodanie pluginu Ruby do asdf
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+​
+# Instalacja zależności dla Ruby
+apt install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev curl
+​
 # Instalacja najnowszej wersji Ruby
-latest_ruby_version=`rbenv install -l | grep -v - | tail -1`
-rbenv install $latest_ruby_version
+latest_ruby_version=`asdf latest ruby`
+asdf install ruby $latest_ruby_version
+asdf global ruby $latest_ruby_version
 
-# Ustawienie najnowszej wersji Ruby jako domyślnej
-rbenv global $latest_ruby_version
+ruby -v
