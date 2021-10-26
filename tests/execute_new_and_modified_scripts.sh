619c7887c53a14661a8e52156007cdb987072357
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 22:11:20 +0200

    Fixed test pipeline privileges

--- tests/execute_new_and_modified_scripts.sh
+++ tests/execute_new_and_modified_scripts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run with root privileges!"
+    exit 1
+fi
+
 latest_main_commit=$(git rev-parse "refs/remotes/origin/main")
 current_commit=$(git rev-parse HEAD)
 
@@ -9,5 +14,9 @@ for item in $scripts_to_be_tested
 do
     echo "Executing $item"
     chmod +x "$item"
-    bash "$item"
+    sudo bash "$item"
+    if [[ $? -ne 0 ]]; then
+        echo "Failed executing $item"
+        exit 1
+    fi
 done
