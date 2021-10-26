884e26b8ef1ac92c1c760d0f126daf4ffcd0e564
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 18:02:20 +0200

    Fixed test script Git issues

--- tests/execute_new_and_modified_scripts.sh
+++ tests/execute_new_and_modified_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-latest_main_commit=$(git log -n 1 --pretty=format:"%H" main)
+latest_main_commit=$(git rev-parse main)
 current_commit=$(git rev-parse HEAD)
 
 scripts_to_be_tested=$(git diff --no-color --name-only "$latest_main_commit" "$current_commit" | grep --color=never \\.sh$ | grep --color=never ^scripts\\/)
