dfe322d2191d05c6d63d10a982ff071e4245536a
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 17:40:24 +0200

    Another test script fix

--- tests/execute_new_and_modified_scripts.sh
+++ tests/execute_new_and_modified_scripts.sh
@@ -1,5 +1,5 @@
-
-for item in `git diff --name-only main..HEAD | grep \\.sh$ | grep ^scripts\\/`
+current_commit=`git rev-parse HEAD`
+for item in `git diff --name-only main..$current_commit | grep \\.sh$ | grep ^scripts\\/`
 do
     chmod +x $item
     bash $item
