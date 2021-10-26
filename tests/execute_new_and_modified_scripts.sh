c11536a59752144bc2d832dfe539c3267040e2bb
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 17:50:04 +0200

    Fixed Git issues in test script

--- tests/execute_new_and_modified_scripts.sh
+++ tests/execute_new_and_modified_scripts.sh
@@ -1,6 +1,6 @@
-git status
+latest_main_commit=`git log -n 1 --pretty=format:"%H"`
 current_commit=`git rev-parse HEAD`
-for item in `git diff --name-only main..$current_commit | grep \\.sh$ | grep ^scripts\\/`
+for item in `git diff --name-only $latest_main_commit $current_commit | grep \\.sh$ | grep ^scripts\\/`
 do
     chmod +x $item
     bash $item
