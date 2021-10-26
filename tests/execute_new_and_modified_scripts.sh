4a3d8d56945cbc97d1ff1648091201db09091ced
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 17:52:13 +0200

    Added information on current script execution

--- tests/execute_new_and_modified_scripts.sh
+++ tests/execute_new_and_modified_scripts.sh
@@ -2,6 +2,7 @@ latest_main_commit=`git log -n 1 --pretty=format:"%H"`
 current_commit=`git rev-parse HEAD`
 for item in `git diff --name-only $latest_main_commit $current_commit | grep \\.sh$ | grep ^scripts\\/`
 do
+    echo "Executing $item"
     chmod +x $item
     bash $item
 done
