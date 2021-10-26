86b1a61935e5c334a166e8fa7a4d8d83fcda559f
Mikołaj Kamiński <mkaminski321@gmail.com>
2021-10-26 17:59:42 +0200

    Test script cleanup

--- tests/execute_new_and_modified_scripts.sh
+++ tests/execute_new_and_modified_scripts.sh
@@ -1,8 +1,13 @@
-latest_main_commit=`git log -n 1 --pretty=format:"%H"`
-current_commit=`git rev-parse HEAD`
-for item in `git diff --name-only $latest_main_commit $current_commit | grep \\.sh$ | grep ^scripts\\/`
+#!/bin/bash
+
+latest_main_commit=$(git log -n 1 --pretty=format:"%H" main)
+current_commit=$(git rev-parse HEAD)
+
+scripts_to_be_tested=$(git diff --no-color --name-only "$latest_main_commit" "$current_commit" | grep --color=never \\.sh$ | grep --color=never ^scripts\\/)
+
+for item in $scripts_to_be_tested
 do
     echo "Executing $item"
-    chmod +x $item
-    bash $item
+    chmod +x "$item"
+    bash "$item"
 done
