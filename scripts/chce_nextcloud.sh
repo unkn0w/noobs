83ae6f7e02fa500f8df0b34224c9b35b9cc29b5d
Mariusz Kowalski <mariusz.kowalski@intel.com>
2023-05-30 22:15:08 +0200

    Update php version to 8.1

--- scripts/chce_nextcloud.sh
+++ scripts/chce_nextcloud.sh
@@ -16,12 +16,12 @@ NEXT_CLOUD_PASS=$(head -c 100 /dev/urandom | tr -dc A-Za-z0-9 | head -c13)
 
 #Installing prerequisites https://docs.nextcloud.com/server/latest/admin_manual/installation/example_ubuntu.html
 apt update
-apt install -y apache2 mariadb-server libapache2-mod-php7.4
-apt install -y php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl
-apt install -y php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip
+apt install -y apache2 mariadb-server libapache2-mod-php8.1
+apt install -y php8.1-gd php8.1-mysql php8.1-curl php8.1-mbstring php8.1-intl
+apt install -y php8.1-gmp php8.1-bcmath php-imagick php8.1-xml php8.1-zip
 
 #Configuring mariaDB
-/etc/init.d/mysql start
+#/etc/init.d/mysql start
 mysql -u"$DB_USER" -p"$DB_PASS" -e "CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASSWORD';
 CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON nextcloud.* TO '$USERNAME'@'localhost';
