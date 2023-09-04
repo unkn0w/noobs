ce182e0405eba431ee1ff7d795f432228ba4db36
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2023-09-04 14:49:01 +0200

    Chce_dotnet - pobieranie najnowszej wersji

--- scripts/chce_DOTNET.sh
+++ scripts/chce_DOTNET.sh
@@ -4,22 +4,24 @@
 #
 
 # Zainstaluj klucze do podpisywania pakietów microsoft
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+latest_version = curl --silent https://packages.microsoft.com/config/ubuntu/ | grep -o 'href=".*">' | sed 's/href="//;s/\/">//' | tail -n 1
+wget https://packages.microsoft.com/config/ubuntu/${latest_version}/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
 sudo dpkg -i /tmp/packages-microsoft-prod.deb
 rm /tmp/packages-microsoft-prod.deb
 
+dotnet_version = 6.0
 # Aktualizacja apt
 sudo apt-get update;
 
-# Instalacja dotnet-sdk-5.0
+# Instalacja dotnet-sdk
 sudo apt-get install -y apt-transport-https && \
     sudo apt-get update && \
-    sudo apt-get install -y dotnet-sdk-5.0
+    sudo apt-get install -y dotnet-sdk-${dotnet_version}
 
 # Instalacia środwiska uruchomieniowego platfromy ASP.NET Core
 sudo apt-get install -y apt-transport-https && \
     sudo apt-get update && \
-    sudo apt-get install -y aspnetcore-runtime-5.0
+    sudo apt-get install -y aspnetcore-runtime-${dotnet_version}
 
 # Lub alternatywa: ASP.NET zmiast ASP.NET Core
-# sudo apt-get install -y dotnet-runtime-5.0
+# sudo apt-get install -y dotnet-runtime-${dotnet_version}
