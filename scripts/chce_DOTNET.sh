96500e7be4b3e6835280fe70ef75bf5d751e65f6
Mateusz Łukasik <mati75@linuxmint.pl>
2023-09-04 17:18:23 +0200

    chce_DOTNET.sh - fix zahardcodowanej wersji #159

--- scripts/chce_DOTNET.sh
+++ scripts/chce_DOTNET.sh
@@ -3,23 +3,40 @@
 # https://docs.microsoft.com/pl-pl/dotnet/core/install/linux-ubuntu
 #
 
+# określenie wersji systemu
+
+usage (){
+    echo "Uzycie: $0 wersja";
+    echo "Musisz podać wersję do zainstalowania np. 7.0";
+    exit 3;
+}
+
+[ "$#" -lt 1 ] && usage
+
+dotnet_version="$1"
+
+apt-get install -y lsb-release
+
+os-version=$(lsb_release -sr)
+
 # Zainstaluj klucze do podpisywania pakietów microsoft
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
-sudo dpkg -i /tmp/packages-microsoft-prod.deb
-rm /tmp/packages-microsoft-prod.deb
+apt-get install -y gpg
+cd /tmp
+wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget https://packages.microsoft.com/config/ubuntu/'$os-version'/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 
 # Aktualizacja apt
-sudo apt-get update;
+apt-get update
 
-# Instalacja dotnet-sdk-5.0
-sudo apt-get install -y apt-transport-https && \
-    sudo apt-get update && \
-    sudo apt-get install -y dotnet-sdk-5.0
+# Instalacja dotnet-sdk
+apt-get install -y dotnet-sdk-"$dotnet_version"
 
 # Instalacia środwiska uruchomieniowego platfromy ASP.NET Core
-sudo apt-get install -y apt-transport-https && \
-    sudo apt-get update && \
-    sudo apt-get install -y aspnetcore-runtime-5.0
+apt-get install -y aspnetcore-runtime-"$dotnet_version"
 
 # Lub alternatywa: ASP.NET zmiast ASP.NET Core
-# sudo apt-get install -y dotnet-runtime-5.0
+# sudo apt-get install -y dotnet-runtime-"$dotnet_version"
