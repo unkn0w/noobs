6b990634ac5092c74dbcd7a8bd7aea8518d6adbf
Mateusz Łukasik <mati75@linuxmint.pl>
2023-09-04 16:51:04 +0200

    chce_LAMP_FastCGI.sh - ustawienie ostatniej dostępnej wersji  #161

--- scripts/chce_LAMP_FastCGI.sh
+++ scripts/chce_LAMP_FastCGI.sh
@@ -4,14 +4,14 @@
 # Edited and modified by: Andrzej 'Ferex' Szczepaniak & Jarosław 'Evilus' Rauza
 
 apt update
-apt install -y software-properties-common 
+apt install -y software-properties-common
 
-# Repozytoria zewnętrzne z PHP 8.0 i najnowszym Apachem (nie ma ich w standardowym Ubuntu)
+# Repozytoria zewnętrzne z PHP i najnowszym Apachem (nie ma ich w standardowym Ubuntu)
 add-apt-repository -y ppa:ondrej/apache2
 add-apt-repository -y ppa:ondrej/php
 
 # apache + najpopularniejsze moduły do PHP oraz memcached
-apt install -y apache2 libapache2-mod-fcgid php8.0 php8.0-fpm php8.0-memcached php8.0-memcache php8.0-zip php8.0-xml php8.0-sqlite3 php8.0-pgsql php8.0-mysql php8.0-mcrypt php8.0-mbstring php8.0-intl php8.0-gd php8.0-curl php8.0-cli php8.0-bcmath
+apt install -y apache2 libapache2-mod-fcgid php php-fpm php-memcached php-memcache php-zip php-xml php-sqlite3 php-pgsql php-mysql php-mcrypt php-mbstring php-intl php-gd php-curl php-cli php-bcmath
 
 # zmiana mpm_prefork na mpm_event, mpm-prefork działa kiedy instalujemy libapache2-mod-php, zaś event dla php-fpm
 a2dismod mpm_prefork
@@ -23,11 +23,12 @@ a2enmod rewrite setenvif proxy proxy_fcgi
 # instalacja memcached
 apt install memcached libmemcached-tools -y
 
-# aktywacja konfiguracji modułu php8.0-fpm dla apache2
-a2enconf php8.0-fpm
+# aktywacja konfiguracji modułu php-fpm dla apache2
+phpversion=$(/usr/bin/php.default -v | head -1 | cut -c5-7)
+a2enconf php"$phpversion"-fpm
 
 # restart usługi po dodaniu nowego modułu
-systemctl restart apache2
+apache2ctl restart
 
 # dodanie MariaDB (klient i serwer)
 apt install -y mariadb-server mariadb-client
@@ -39,7 +40,7 @@ systemctl start mariadb
 systemctl enable apache2
 systemctl enable mariadb
 
-# Usuwamy domyślną 
+# Usuwamy domyślną
 rm /var/www/html/index.html
 
 # Dowód na działanie PHP
