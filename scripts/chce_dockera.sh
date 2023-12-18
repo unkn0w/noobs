2366397682f97cabd8dbb9a494f25b8b497aff48
Bartlomiej Mankowski <bmankowski@gmail.com>
2023-12-18 17:32:56 +0100

    Update chce_dockera.sh
    Poprawka literówki.

--- scripts/chce_dockera.sh
+++ scripts/chce_dockera.sh
@@ -14,7 +14,7 @@ apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 # Dodanie oficjalnego klucza GPG Dockera
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Dodanie oficjalnych repozytorium Dockera do systmeu
+# Dodanie oficjalnych repozytorium Dockera do systemu
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
