010747fa69d886dbcbcc4993a04d21fcc37f63be
Andrzej Szczepaniak <git@andrzejszczepaniak.co.uk>
2026-01-04 18:35:41 +0100

    issue_205

--- scripts/chce_webmina.sh
+++ scripts/chce_webmina.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Author: Marcin 'y0rune' Wozniak
+# Edited and modified by: Andrzej 'Ferex' Szczepaniak
 #
 
 # Check if you are root
@@ -9,21 +10,9 @@
 # Configuring tzdata if not exist
 [[ ! -f /etc/localtime ]] && ln -fs /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 
-# Install all missing dependencies
-apt-get update
-apt-get install curl wget perl libnet-ssleay-perl openssl libauthen-pam-perl \
-	libpam-runtime libio-pty-perl apt-show-versions python unzip \
-	shared-mime-info -y
-
-# Values
-webadmin_tmp="/tmp/webadmin.deb"
-latest=$(curl https://www.webmin.com | grep -Eo 'http://.+.deb')
-
-# Downloading
-wget "$latest" -O "$webadmin_tmp"
-
-# Installation
-dpkg -i "$webadmin_tmp"
+curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+bash webmin-setup-repo.sh -f
+apt install webmin --install-recommends -y
 
 # Configuration
 port_number=$(echo -e "30$(hostname | grep -Eo '[0-9]{3}')")
@@ -31,7 +20,4 @@ sed -i "s|port=10000|port=$port_number|" /etc/webmin/miniserv.conf
 sed -i "s|listen=10000|listen=$port_number|" /etc/webmin/miniserv.conf
 
 # Restart
-/etc/init.d/webmin restart
-
-# Remove tmp file
-rm "$webadmin_tmp"
+systemctl restart webmin
